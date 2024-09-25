db92b9020abc0c8bdbbe43dc62124fc131e03b08
Erwann Le Corre <lcerwann@gmail.com>
2024-09-25 13:46:51 +0200

    feat legal page

---
+++ app/(public)/legal/licenses/FontAwesomeAtttribution.tsx
@@ -0,0 +1,11 @@
+export default function FontAwesomeAtttribution() {
+  return (
+    <p>
+      This project uses icons from Font Awesome Free, which are licensed under the 
+      <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">
+        Creative Commons Attribution 4.0 International License (CC BY 4.0)</a>.
+      You can find more about Font Awesome at 
+        <a href="https://fontawesome.com" target="_blank">Font Awesome</a>.
+    </p>
+  );
+}
