a4a24447aaf6d8faf9823c767243fcb566bc63b4
Erwann Le Corre <lcerwann@gmail.com>
2024-09-25 13:44:49 +0200

    update MY_COORDS

--- constants/my_coords.ts
+++ constants/my_coords.ts
@@ -7,13 +7,13 @@ const MY_COORDS = {
   MY_FULLNAME: `${MY_NAME.FIRSTNAME} ${MY_NAME.LASTNAME}`,
   MY_MAIL: "myemail@mydomain.com",
   MY_ADDRESS: "mon adresse",
-  MY_TEL: "mon tel",
-  MY_SITE: "mon site",
-  MY_FB: "lien fb",
-  MY_INSTA: "lien insta",
+  MY_TEL: "0601020304",
+  MY_SITE: "monsite.com",
+  MY_FB: "https://www.facebook.com/",
+  MY_INSTA: "https://www.instagram.com/",
 };
 
-const SITE_TITLE = "Mon nouveau site";
+const SITE_TITLE = "NOUVEAU SITE";
 
 export {
   MY_COORDS,
